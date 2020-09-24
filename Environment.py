272ac2e83449b443ac73253152024f5ab4a149b9
Gleb Kuzmin <glkuzi@mail.ru>
2020-09-24 12:33:39 +0300

    added environment choice

--- Environment.py
+++ Environment.py
@@ -24,7 +24,7 @@ class ReplayBuffer:
 
     def sample(self, batch_size):
         random_idx = random.randint(0, len(self.Buffer) - batch_size)
-        mini_batch = self.Buffer[random_idx : random_idx + batch_size]
+        mini_batch = self.Buffer[random_idx: random_idx + batch_size]
 
         s_batch, a_batch, r_batch, t_batch = [], [], [], []
         for transition in mini_batch:
@@ -34,30 +34,31 @@ class ReplayBuffer:
             a_batch.append(a)
             r_batch.append(r)
             t_batch.append(t)
-        
+
         return s_batch, a_batch, r_batch, t_batch
 
     def size(self):
         return len(self.Buffer)
 
-class breakout:
-    def __init__(self, height, width):
-        self.env = gym.make('Breakout-v0')
+
+class atari_env:
+    def __init__(self, height, width, name):
+        self.env = gym.make(name)
         self.height = height
         self.width = width
-    
+
     def pre_process(self, s):
         """
         preprocessing the given image, gray scailing and change it to 84 x 84
 
         input:
-          s -- given state, RGB image of 210 * 160 *3 
+          s -- given state, RGB image of 210 * 160 *3
         """
         s = cv2.cvtColor(cv2.resize(s, (self.height, self.width)), cv2.COLOR_BGR2GRAY)
         s = np.divide(s, 255.0)
         s = np.expand_dims(s, axis=0)
         return s
-    
+
     def step(self, a):
         s_prime, r, terminate, info = self.env.step(a)
         s_prime = self.pre_process(s_prime)
@@ -67,10 +68,9 @@ class breakout:
         s = self.env.reset()
         s = self.pre_process(s)
         return s
-    
+
     def render(self):
         self.env.render()
 
     def close(self):
         self.env.close()
-
